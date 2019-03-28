2973cc58b6f7ac8ff0f7059be6dc811bbebd2a9a
oi <oi@Ogors-MacBook-Pro.local>
2019-03-28 10:42:17 -0500

    install third party libraries - 'React-Native-Vecttor-Icon' and 'React-Navigation'

--- App.js
+++ App.js
@@ -1,72 +1,47 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import PlaceInput from './src/components/place-input/PlaceInput';
 import PlaceList from './src/components/place-list/PlaceList';
 //import placeImage from './src/assets/img/maldives.jpeg';
 import PlaceDetail from './src/components/place-detail/PlaceDetail';
+import { 
+  addPlace,
+  selectPlace,
+  deletePlace,
+  deselectPlace } from './src/store/actions/index';
 
 
-export default class App extends Component {
-  state = {
-    places: [],
-    selectedPlace: null
-  };
-
+class App extends Component {
 
   placeAddedHandler = placeName => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random(),
-          name: placeName,
-          image: {
-            uri: "https://images.unsplash.com/photo-1553345316-84f303d0669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-          }
-        })
-      };
-    });
-  };
-
-
-  placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key;
-        })
-      };
-    });
+    this.props.onAddPlace(placeName);
   };
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace()
   };
 
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
-  }
+    this.props.onDeselectPlace();
+  };
+  
+  placeSelectedHandler = key => {
+    this.props.onSelectPlace(key);
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-          selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}  
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
       />
       </View>
@@ -83,3 +58,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   }
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: name => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: key => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- android/app/src/main/java/com/basicapp/MainApplication.java
+++ android/app/src/main/java/com/basicapp/MainApplication.java
@@ -7,13 +7,15 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
+  /*private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -22,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new VectorIconsPackage()
       );
     }
 
@@ -40,6 +43,30 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, *//* native exopackage */ //false);
+  //}
+
+  @Override
+    public boolean isDebug() {
+      // Make sure you are using BuildConfig from your own application
+      return BuildConfig.DEBUG;
+    }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+    new VectorIconsPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
   }
 }
